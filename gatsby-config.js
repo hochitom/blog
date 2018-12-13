c690cf600859a6c4bc1bcd85feb2d2940b967944
Thomas Hochoertler <hochitom@me.com>
2018-12-13 16:32:27 +0800

    updated site meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'hochitom.at',
+    description: 'Thomas Hochörtler ist ein profesioneller Frontend-Developer aus Österreich. Auf seinem persönlichen Blog schreibt er über Themen die ihn interessieren.',
+    author: '@hochitom',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
