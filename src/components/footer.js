65ddef8dfa77aa76056e58d1ffb39e4345b3d08e
Thomas Hochoertler <hochitom@me.com>
2018-12-17 17:28:15 +0800

    responsive layout fixes

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import './branding.css'
 
 const Footer = () => (
   <div class="branding branding--inverse">
-    <div class="wrapper">
+    <div className="wrapper">
       <p style={{ margin: 0 }}>made with <span role="img" aria-label="todo">❤</span>️ in Austria | Kontakt, Impressum, Datenschutz<br />powered by gatsby and wordpress.</p>
     </div>
   </div>
