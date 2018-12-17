7347bd64959d89ce18b86f88c3381bb4c679bbd3
Thomas Hochoertler <hochitom@me.com>
2018-12-17 10:42:32 +0800

    added manifest data

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,18 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     {
-      resolve: `gatsby-plugin-manifest`
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "hochitom.at - personal Blog",
+        short_name: "hochitom.at",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
     },
     'gatsby-plugin-offline'
   ],
