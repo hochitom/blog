0074c3a66631f57df8b310a8dac72e68879b3335
Thomas Hochoertler <hochitom@me.com>
2018-12-19 18:27:26 +0800

    added Links

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ import './branding.css'
 const Footer = () => (
   <div class="branding branding--inverse">
     <div className="wrapper">
-      <p style={{ margin: 0 }}>made with <span role="img" aria-label="heart">❤</span>️ in Austria | Kontakt, <Link to="/impressum-datenschutzerklaerung/">Impressum &amp; Datenschutz</Link><br /><span role="img" aria-label="rocket">🚀</span>️ powered by gatsby and wordpress.</p>
+      <p style={{ margin: 0 }}>made with <span role="img" aria-label="heart">❤️</span>️ in Austria | Kontakt, <Link to="/impressum-datenschutzerklaerung/">Impressum &amp; Datenschutz</Link><br /><span role="img" aria-label="rocket">🚀</span>️ powered by <a href="https://www.gatsbyjs.org/" rel="nofollow">gatsby</a> and <a href="https://wordpress.org/" rel="nofollow">wordpress</a>.</p>
     </div>
   </div>
 )
