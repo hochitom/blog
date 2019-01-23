a26a9aca90ae37d238900849ea84350beb6a2115
Thomas Hochoertler <hochitom@me.com>
2019-01-23 21:16:21 +0800

    load and display base data

--- src/components/map.js
+++ src/components/map.js
@@ -1,24 +1,39 @@
 import React from 'react'
-import fetch from 'node-fetch'
 
-const mapApiUrl = '/.netlify/functions/getGpsiesTrackData';
-const getTrackData = (fieldId) => {
-  fetch( `${mapApiUrl}?fieldId=${fieldId}`).then((res) => {
-    console.log(res);
-    this.trackData = res;
-  });
-};
+const mapApiUrl = `/.netlify/functions/getGpsiesTrackData`;
+// const getTrackData = (fieldId) => {
+//   fetch( `${mapApiUrl}?fieldId=${fieldId}`)
+//   .then(response => response.json())
+//   .then((data) => {
+//     console.log(data.track);
+//     this.trackData = data.track;
+//   });
+// };
 
 export default class Map extends React.Component {
   trackId = this.props.id;
+  trackDataLoaded = false;
 
-  trackData = {}
+  state = {}
+
+  getTrackData(fieldId) {
+    fetch( `${mapApiUrl}?fieldId=${fieldId}`)
+      .then(response => response.json())
+      .then((data) => {
+        this.trackDataLoaded = true;
+        this.setState(data.track);
+      });
+  }
 
   render() {
-    getTrackData(this.trackId);
-
+    if (this.trackDataLoaded === false) {
+      this.getTrackData(this.trackId);
+    }
     return (
       <div>
+        <h2>{this.state.title}</h2>
+        <p>{ (this.state.trackLengthM / 1000).toFixed(2)} km</p>
+        <p>{this.state.totalAscentM} m</p>
         <a href={`http://www.gpsies.com/map.do?fileId=${this.trackId}`}>Tour auf gpsies.com anschauen</a>
       </div>
     )
