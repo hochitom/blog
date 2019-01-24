261eaa2205356442d0f6728c6270dd5c5160bca8
Thomas Hochoertler <hochitom@me.com>
2019-01-24 11:53:47 +0800

    added lambda function to get strava activity

---
+++ src/lambda/getStravaActivity.js
@@ -0,0 +1,30 @@
+const rp = require('request-promise');
+const response = require('./lib/response');
+const cache = require('./lib/cache');
+
+const accessToken = "63397d5e9040aa2fc00140f5de2e39fa01f2b2cf";
+
+exports.handler = (event, context, callback) => {
+  const activityId = event.queryStringParameters.activityId;
+  const trackData = cache.get(activityId);
+
+  if (trackData !== false) {
+    return response(callback, trackData);
+  }
+
+  rp
+    .get(`https://www.strava.com/api/v3/activities/${activityId}?include_all_efforts=false`, {
+      'auth': {
+        'bearer': accessToken
+      }
+    })
+    .then((res) => {
+      console.dir(res);
+      cache.add(activityId, res);
+      response(callback, res);
+    })
+    .catch((err) => {
+      console.error(err);
+      response(callback, err);
+    });
+}
