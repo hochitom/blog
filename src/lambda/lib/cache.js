83b294b539fa3c4ace0267bc32591a8a19dfebbb
Thomas Hochoertler <hochitom@me.com>
2019-01-24 12:04:41 +0800

    changed cachehit

--- src/lambda/lib/cache.js
+++ src/lambda/lib/cache.js
@@ -1,12 +1,12 @@
 let dumbCache = {};
 
 const addToCache = (key, data) => {
-  data.cacheHit = true;
   dumbCache[key] = data;
 }
 
 const getFromCache = (key) => {
   if (dumbCache[key] !== undefined) {
+    console.log(`cache hit: ${key}`);
     return dumbCache[key];
   } else {
     return false;
