d728cdad6aaf35743b26cea479436dcbafdfe110
Thomas Hochoertler <hochitom@me.com>
2019-01-25 11:29:52 +0800

    add bounds to response

--- src/lambda/getStravaActivity.js
+++ src/lambda/getStravaActivity.js
@@ -9,6 +9,29 @@ const cache = require('./lib/cache');
 
 const accessToken = "34c98946209aa94e3465c0908732b55b858c0801";
 
+const extractBounds = (coords) => {
+  let s = 100;
+  let w = 100;
+  let n = 0;
+  let e = 0;
+
+  coords.forEach((c) => {
+    if (s > c[1]) s = c[1];
+    if (n < c[1]) n = c[1];
+    if (e < c[0]) e = c[0];
+    if (w > c[0]) w = c[0];
+  })
+
+  const bounds = {
+    'w': w,
+    's': s,
+    'e': e,
+    'n': n
+  }
+
+  return [w, n, e, s];
+}
+
 exports.handler = (event, context, callback) => {
   const activityId = event.queryStringParameters.id;
   const trackData = cache.get(activityId);
@@ -26,6 +49,7 @@ exports.handler = (event, context, callback) => {
     .then((res) => {
       let data = JSON.parse(res);
       data.geoJson = polyline.decode(data.map.polyline).map((latLng) => [latLng[1], latLng[0]])
+      data.bounds = extractBounds(data.geoJson)
       data = JSON.stringify(data);
       cache.add(activityId, data);
       response(callback, data);
