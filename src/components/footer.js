61009a0981528a1c48ea829663ef35841c63789a
Thomas Hochoertler <hochitom@me.com>
2019-01-25 08:51:36 +0800

    fixed attribute

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 import './branding.css'
 
 const Footer = () => (
-  <div class="branding branding--inverse">
+  <div className="branding branding--inverse">
     <div className="wrapper">
       <p style={{ margin: 0 }}>made with <span role="img" aria-label="heart">❤️</span>️ in Austria | Kontakt, <Link to="/impressum-datenschutzerklaerung/">Impressum &amp; Datenschutz</Link><br /><span role="img" aria-label="rocket">🚀</span>️ powered by <a href="https://www.gatsbyjs.org/" rel="nofollow">gatsby</a> and <a href="https://wordpress.org/" rel="nofollow">wordpress</a>.</p>
     </div>
